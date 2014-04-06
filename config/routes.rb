27abec6d0193d35bfea5aabeb7a8249ea73b378c
Riad G. Jaradeh <jaradehr1@mail.montclair.edu>
2014-04-06 12:15:23 -0400

    My Update On User MVC - Riad

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 WebTutor::Application.routes.draw do
-  get "access/index"
-
-  get "access/login"
-
-  resources :users
-
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
