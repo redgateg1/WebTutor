f910927bb3cae6718b5fa04d164f14eadceb782d
Frank Paladino <fjp0204@yahoo.com>
2014-04-10 23:56:06 -0400

    fixed mass assignment issue with authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
-	#has_secure_password
-  attr_accessible :first_name, :last_name, :password_digest, :subscribtion_expire, :user_role, :username
+  has_secure_password
+  attr_accessible :first_name, :last_name, :password, :subscribtion_expire, :user_role, :username
+
+
 end
