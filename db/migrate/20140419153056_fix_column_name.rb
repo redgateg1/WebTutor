28fb9a5d8ec81039c6434d99819827556df46098
Gabby <redgateg1@mail.montclair.edu>
2014-05-04 12:46:03 -0300

    fixed something for heroku

--- db/migrate/20140419153056_fix_column_name.rb
+++
@@ -1,9 +0,0 @@
-class FixColumnName < ActiveRecord::Migration
-  def up
-    rename_column :tutorials, :category_id, :CategoryID
-    rename_column :tutorials, :tutorial_type, :TutorialType
-  end
-
-  def down
-  end
-end
